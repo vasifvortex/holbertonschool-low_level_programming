58aca2ea5435e31574719d8be9b5867ee4002cf6
root <vasif.oruczade@gmail.com>
2023-10-17 09:53:25 -0700

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c<='z' ; c++)
+if(c != 'e' && c != 'q')
+putchar(c);
+putchar('\n');
+return (0);
+}
