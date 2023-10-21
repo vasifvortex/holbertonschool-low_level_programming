a4f386c533cea16d8e34548bc301064e1f636456
root <vasif.oruczade@gmail.com>
2023-10-21 11:04:36 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+char c = 'a';
+for(c = 'a';c <= 'z';c++)
+_putchar(c)
+_putchar('\n');
+}
