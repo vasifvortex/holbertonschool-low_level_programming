726c3e4768c10b6375bff204af807e5bb7372698
root <vasif.oruczade@gmail.com>
2023-10-21 05:18:18 -0700

    8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 int i;
 char letter;
 for (i = 0; i < 10; i++)
-putchar((i % 10) +'0');
+putchar((i % 10) + '0');
 for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
