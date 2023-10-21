d6634fe0b043b694ba61c5cb1100c56a27ff562e
root <vasif.oruczade@gmail.com>
2023-10-21 11:20:40 -0700

    3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lowercase character
  * @c: the character
- * Return: 1 if c is lowercase, 0 otherwise 
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
