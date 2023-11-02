ba762118a596ae77023bc6609657141b9e5c026a
root <vasif.oruczade@gmail.com>
2023-11-02 06:31:38 -0700

    1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,14 +5,14 @@
  *    *
  *     * Return: On success 1.
  *      * On error, -1 is returned, and errno is set appropriately.
- *       */
+*/
 void _print_rev_recursion(char *s)
 {
-		if (*s != '\0')
-				{
-							_print_rev_recursion(s + 1);
-									_putchar(*s);
-										}
+	if (*s != '\0')
+		{
+ 			_print_rev_recursion(s + 1);
+			_putchar(*s);
+		}
 
 }
 
