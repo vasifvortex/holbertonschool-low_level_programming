66a7f6c4c33f42488bd9a4870f772fea94201c88
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-15 00:24:49 -0800

    3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **alloc_grid - function to allocate memory to grid
+ * @width: int type
+ * @height: int type
+ * Return: grid of 0s
+ */
+int **alloc_grid(int width, int height)
+{
+if (width <= 0 || height <= 0)
+{
+int **grid;
+return (NULL);
+}
+grid = (int **)malloc(height * sizeof(int *));
+if (grid == NULL)
+{
+return (NULL);
+}
+for (int i = 0; i < height; i++)
+{
+grid[i] = (int *)malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+for (int j = 0; j < i; j++)
+{
+free(grid[j]);
+}
+free(grid);
+return (NULL);
+}
+for (int j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+}
+return (grid);
+}
