51e81df6efa437469e0ad2e15464e7a91d734104
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-15 00:08:27 -0800

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,7 +3,8 @@
 #include <string.h>
 char *str_concat(char *s1, char *s2)
 {
-  size_t len_s1,len_s2;  
+size_t len_s1,len_s2;  
+char *result;
 if (s1 == NULL)
 {
 s1 = "";
@@ -14,7 +15,7 @@ s2 = "";
 }
 len_s1 = strlen(s1);
 len_s2 = strlen(s2);
-char *result = (char *)malloc(len_s1 + len_s2 + 1);
+result = (char *)malloc(len_s1 + len_s2 + 1);
 if (result == NULL)
 {
 return NULL;
