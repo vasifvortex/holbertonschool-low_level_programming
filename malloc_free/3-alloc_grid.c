f3c74db4c7bcf2e776615d7d7c2c473c02ab9bed
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-15 00:34:24 -0800

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -8,6 +8,7 @@
  */
 int **alloc_grid(int width, int height)
 {
+int i, j;
 if (width <= 0 || height <= 0)
 {
 return (NULL);
@@ -17,13 +18,11 @@ if (grid == NULL)
 {
 return (NULL);
 }
-int i;
 for (i = 0; i < height; i++)
 {
 grid[i] = (int *)malloc(width * sizeof(int));
 if (grid[i] == NULL)
 {
-int j;
 for (j = 0; j < i; j++)
 {
 free(grid[j]);
@@ -31,7 +30,7 @@ free(grid[j]);
 free(grid);
 return (NULL);
 }
-for (int j = 0; j < width; j++)
+for (j = 0; j < width; j++)
 {
 grid[i][j] = 0;
 }
