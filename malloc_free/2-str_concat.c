85810e10c2e604ac73c98c4be77d06ad687136e4
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-15 00:13:57 -0800

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
+ */
 char *str_concat(char *s1, char *s2)
 {
-size_t len_s1,len_s2;  
+size_t len_s1, len_s2;
 char *result;
 if (s1 == NULL)
 {
@@ -18,9 +24,9 @@ len_s2 = strlen(s2);
 result = (char *)malloc(len_s1 + len_s2 + 1);
 if (result == NULL)
 {
-return NULL;
+return (NULL);
 }
 strcpy(result, s1);
 strcat(result, s2);
-return result;
+return (result);
 }
