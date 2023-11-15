7119101370967b9a12252929658553f260e9739a
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-15 01:26:26 -0800

    1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - concat 
+ *@s1: char
+ *@s2: char
+ *@n: unsigned int
+ * Return: A pointer.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *result;
+unsigned int len_s1, len_s2;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len_s1 = strlen(s1);
+len_s2 = strlen(s2);
+if (n >= len_s2)
+n = len_s2;
+result = (char *)malloc(len_s1 + n + 1);
+if (result == NULL)
+return (NULL);
+strcpy(result, s1);
+strncat(result, s2, n);
+result[len_s1 + n] = '\0';
+return (result);
+}
