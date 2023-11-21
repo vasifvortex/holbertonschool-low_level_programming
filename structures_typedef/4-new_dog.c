534bcbc374597edcff43857c10c5581321e7ca4f
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-21 14:28:58 -0800

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog_ptr;
-new_dog_ptr = malloc(sizeof(dog_t));
+new_dog_ptr = malloc(sizeof(dog_t) + 1);
 if (new_dog_ptr == NULL)
 return (NULL);
 new_dog_ptr->name = strdup(name);
