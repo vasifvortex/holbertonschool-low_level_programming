3c85359fce7caf9bc397f39b6aa57932c0d7283a
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-21 14:08:02 -0800

    4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the new dog, or NULL if the function fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new_dog_ptr;
+new_dog_ptr = malloc(sizeof(dog_t));
+if (new_dog_ptr == NULL)
+return (NULL);
+new_dog_ptr->name = strdup(name);
+new_dog_ptr->owner = strdup(owner);
+if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
+{
+free(new_dog_ptr->name);
+free(new_dog_ptr->owner);
+free(new_dog_ptr);
+return (NULL);
+}
+new_dog_ptr->age = age;
+return (new_dog_ptr);
+}
