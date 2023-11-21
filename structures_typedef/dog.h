ef3af2007b8e9627e6c8e1b0a221f03e8c573b95
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-21 05:26:42 -0800

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog (a string).
+ * @age: The age of the dog (a floating-point number).
+ * @owner: The owner of the dog (a string).
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
