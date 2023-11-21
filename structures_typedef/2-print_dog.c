0e4860575728d7fb947f0445691cf53d0baccbf5
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-21 12:28:41 -0800

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,10 +7,10 @@
  */
 void print_dog(struct dog *d)
 {
-if(d != NULL)
+if (d != NULL)
 {
-  printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-  printf("Age: %0.6f\n",d->age);
-  printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+printf("Age: %0.6f\n", d->age);
+printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
 }
