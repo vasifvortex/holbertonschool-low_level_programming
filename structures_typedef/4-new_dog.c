f1a476d46f633c9c817b871951fbc3e97903693f
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-24 05:21:02 -0800

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,17 +16,34 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *new_dog_ptr;
 new_dog_ptr = malloc(sizeof(dog_t));
 if (new_dog_ptr == NULL)
+{
+new_dog_ptr = malloc(sizeof(dog_t));
+if (new_dog_ptr == NULL)
 return (NULL);
+new_dog_ptr->name = NULL;
+new_dog_ptr->owner = NULL;
+new_dog_ptr->age = 0.0;
+}
+else
+{
 new_dog_ptr->name = strdup(name);
 new_dog_ptr->owner = strdup(owner);
 if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
 {
 free(new_dog_ptr->name);
 free(new_dog_ptr->owner);
-free((dog_t *)&new_dog_ptr->age);
 free(new_dog_ptr);
+new_dog_ptr = malloc(sizeof(dog_t));
+if (new_dog_ptr == NULL)
 return (NULL);
+new_dog_ptr->name = NULL;
+new_dog_ptr->owner = NULL;
+new_dog_ptr->age = 0.0;
 }
+else
+{
 new_dog_ptr->age = age;
+}
+}
 return (new_dog_ptr);
 }
