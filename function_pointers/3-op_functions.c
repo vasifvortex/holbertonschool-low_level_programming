b0a6c5bfed874412884c33e156cc23044b1ae46a
vasifvortex <Vasif.oruczade@gmail.com>
2023-11-25 08:43:03 -0800

    3.2

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "calc.h"
+
+/**
+ * op_add - returns the sum of a and b.
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: Sum of a and b.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b.
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: Difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b.
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: Product of a and b.
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - returns the result of the division of a by b.
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: Result of the division of a by b.
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b.
+ * @a: The first operand
+ * @b: The second operand
+ *
+ * Return: Remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
+}
