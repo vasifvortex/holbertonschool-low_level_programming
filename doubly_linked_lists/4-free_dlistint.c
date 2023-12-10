48a7baf68f0e0a429877050d99bad739ce6500bc
vasifvortex <Vasif.oruczade@gmail.com>
2023-12-10 07:02:34 -0800

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: A pointer to the head of the doubly linked list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *current, *next;
+current = head;
+while (current != NULL)
+{
+next = current->next;
+free(current);
+current = next;
+}
+}
