ab0c171fd72f2ba63a0b52c7512ca67d482e2cc7
vasifvortex <Vasif.oruczade@gmail.com>
2023-12-17 06:00:59 -0800

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,68 +1,102 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-#define BUFFER_SIZE 1024
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
 /**
- * main - Copies the content of one file to another.
- *
- * @argc: Number of arguments.
- * @argv: Array of argument strings.
- *
- * Return: 0 on success, or the corresponding exit code on failure.
+ * args_checker - checks if the number of arguments is correct
+ * @cnt: number of arguments
+ * Return: void
  */
-int main(int argc, char *argv[])
+void args_checker(int cnt)
 {
-int fd_from, fd_to;
-ssize_t bytes_read, bytes_written;
-char buffer[BUFFER_SIZE];
-if (argc != 3)
+if (cnt != 3)
 {
-dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
+}
+/**
+ * file_from_checker - checks if the file_from can be readed
+ * @checked_file: return value of write
+ * @filename: name of the file
+ * Return: void
+ */
+void file_from_checker(int checked_file, char *filename)
 {
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+if (checked_file == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+close(checked_file);
 exit(98);
 }
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-if (fd_to == -1)
+}
+/**
+ * file_to_checker - checks if the file_to can be written
+ * @checked_file: return value of write
+ * @filename: name of the file
+ * Return: void
+ */
+void file_to_checker(int checked_file, char *filename)
 {
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
+if (checked_file == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+close(checked_file);
 exit(99);
 }
-while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+}
+/**
+ * check_close - checks if the file descriptor can be closed
+ * @closed_file: int
+ * @fd: file descriptor
+ * Return: void
+ */
+void check_close(int closed_file, int fd)
 {
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1)
+if (closed_file == -1)
 {
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-close(fd_to);
-exit(99);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
 }
 }
-if (bytes_read == -1)
+/**
+ * main - linux cp command
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
 {
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+mode_t mask;
+ssize_t rlen, wlen;
+int fd_from, fd_to, close_to, close_from;
+char buffer[1024];
+args_checker(ac);
+mask = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+fd_from = open(av[1], O_RDONLY);
+file_from_checker(fd_from, av[1]);
+fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, mask);
+file_to_checker(fd_to, av[2]);
+while ((rlen = read(fd_from, buffer, 1024)) > 0)
+{
+file_from_checker(fd_from, av[1]);
+wlen = write(fd_to, buffer, rlen);
+if (rlen != wlen)
+fd_to = -1;
+file_to_checker(fd_to, av[2]);
+}
+if (rlen == -1)
+{
 close(fd_from);
 close(fd_to);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 exit(98);
 }
-if (close(fd_from) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd_from);
-exit(100);
-}
-if (close(fd_to) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd_to);
-exit(100);
-}
+close_from = close(fd_from);
+close_to = close(fd_to);
+check_close(close_from, fd_from);
+check_close(close_to, fd_to);
 return (0);
 }
