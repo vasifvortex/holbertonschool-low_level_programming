8df53f3d66c5e48c930993c22e73427535c6523e
vasifvortex <Vasif.oruczade@gmail.com>
2023-12-17 05:31:39 -0800

    1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ *
+ * @filename: The name of the file to create.
+ * @text_content: A NULL-terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+ssize_t bytes_written;
+int fd;
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
+{
+bytes_written = write(fd, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
