cfe496080b4264e80ff45a02b97b0d57ce2166a8
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-01-20 14:05:20 +0400

    Update search_algos.h

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -5,5 +5,5 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+void binary_printer(int *arr, size_t mid, size_t size);
 #endif /* SEARCH_ALGOS_H */
